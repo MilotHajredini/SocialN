92df8d79d1fa9668867321485b6ddec391d820d1
Milot <m.hajredini6@gmail.com>
2019-08-23 10:56:11 -0400

    Profile,Experience,Education,Post
    crud

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,14 +8,20 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
-          <i className='fas fa-user' />
+          <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
         </Link>
       </li>
       <li>
         <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt' />
+          <i className='fas fa-sign-out-alt' />{' '}
           <span className='hide-sm'>LogOut</span>
         </a>
       </li>
@@ -25,7 +31,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers</a>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
